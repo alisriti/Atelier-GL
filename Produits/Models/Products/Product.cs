fb3425e71093964c3c78db8b6aac09e7207f7088
ali sriti <alisriti@live.fr>
2022-01-18 23:07:13 +0000

    Product Model

---
+++ Produits/Models/Products/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Produits.Models.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Designation { get; set; }
+        public int Quantite { get; set; }
+        public int Prix { get; set; }
+    }
+}
