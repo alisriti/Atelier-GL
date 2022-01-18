bb4df93367a7fe56b967b12fd7581579cfb17dea
ali sriti <alisriti@live.fr>
2022-01-18 23:17:34 +0000

    Product Mapper from DataRow

---
+++ Produits/Models/Products/ProductMapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace Produits.Models.Products
+{
+    public static class ProductMapper
+    {
+        public static Product ToProduct(this DataRow row)
+        {
+            return new Product()
+            {
+                Id = (int)row["Id"],
+                Designation = (string)row["Designation"],
+                Quantite = (int)row["Quantite"],
+                Prix = (int)row["Prix"]
+            };
+        }
+    }
+}
