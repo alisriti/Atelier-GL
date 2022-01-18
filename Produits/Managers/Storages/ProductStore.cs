bdc5676eec4b5ff6c50366d939b3ef12cbae4218
ali sriti <alisriti@live.fr>
2022-01-18 22:50:35 +0000

    Implement GetProducts in concrete class ProductStore

---
+++ Produits/Managers/Storages/ProductStore.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Produits.Managers.Storages
+{
+    public class ProductStore : IProductStore
+    {
+        private readonly IConfiguration configuration;
+
+        public ProductStore(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public DataTable GetProducts()
+        {
+            DataTable tableProduits = new DataTable();
+            string connectionString = configuration.GetConnectionString("depinfo");
+            connectionString = configuration.GetConnectionString("depinfo");
+            using var connection = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM PRODUITS", connection);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            connection.Open();
+            da.Fill(tableProduits);
+            return tableProduits;
+        }
+    }
+}
